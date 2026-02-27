81b3dd1e8173ee54991667f7905617aaa73614a9
blake-ledford <cameron.blake.ledford@gmail.com>
2026-02-27 09:15:03 -0500

    Update ballon_popper.py
    pop is working and resetting size, it is currently listening for button instead of moue click

--- python_tutorials/ballon_popper.py
+++ python_tutorials/ballon_popper.py
@@ -5,11 +5,21 @@ pop_diamter = 100 #pixels
 
 def draw_balloon ():
     color("red")
-    begin_fill()
     dot(diameter)
-    end_fill()
+
+def inflate_balloon ():
+    global diameter #gives access to vars in global scope
+    diameter = diameter + 10
+    draw_balloon()
+
+    if diameter >= pop_diamter:
+        clear()
+        diameter = 40
+        write("Pop!")
 
 draw_balloon() #function call
 
+onkey(inflate_balloon, "space")
+listen()
 #finish turtle commands
 done()
