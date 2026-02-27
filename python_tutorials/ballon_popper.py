df71a86710469e260f88a1d9295045092d8311c2
blake-ledford <cameron.blake.ledford@gmail.com>
2026-02-27 09:04:43 -0500

    working  first step
    First step of balloon popper is working

---
+++ python_tutorials/ballon_popper.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+diameter = 40 #pixels
+pop_diamter = 100 #pixels
+
+def draw_balloon ():
+    color("red")
+    begin_fill()
+    dot(diameter)
+    end_fill()
+
+draw_balloon() #function call
+
+#finish turtle commands
+done()
